9130fbd610290426c0eddff65480e55d1f110812
DavMark2909 <77192509+DavMark2909@users.noreply.github.com>
2024-09-14 23:04:38 -0400

    added book entity and repository/service

---
+++ src/main/java/catalogservice/domain/exception/BookAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package catalogservice.domain.exception;
+
+public class BookAlreadyExistsException extends RuntimeException {
+
+    public BookAlreadyExistsException(String isbn) {
+        super("A book with ISBN " + isbn + " already exists.");
+    }
+
+}
